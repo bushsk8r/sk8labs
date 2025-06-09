dad62cbaf2b3dfd5b7532712c73f097687d07ae4
bushsk8r <96554498+bushsk8r@users.noreply.github.com>
2025-06-08 17:45:40 -0700

    present index and script

---
+++ gallery/present/present.js
@@ -0,0 +1,52 @@
+import {
+  createElement,
+  addToTag,
+  createContainer,
+  createLink,
+} from "../helper.js";
+
+//data to be displayed
+const studies = [
+  {
+    title: "minesweeper",
+    description: [
+      "mobile app development",
+      "game development with react native",
+    ],
+  },
+  {
+    title: "hypermedia zine",
+    description: [
+      "editorial media",
+      "full stack web development using htmx and rust",
+    ],
+  },
+];
+
+//document main section
+const main = document.querySelector("main");
+
+//page title element
+const title = createElement("h1", "Present curiosities", []);
+
+//map data into item cards
+const displayCard = studies.map((study) => {
+  //item title element
+  const studyTitle = createElement("h2", study.title, []);
+
+  //map item details into elements array
+  const studyDetails = study.description.map((desc) => {
+    return createElement("li", `a study of ${desc}`, []);
+  });
+
+  //container with title and item details
+  return createContainer(
+    "section",
+    "",
+    [],
+    [studyTitle, createContainer("ul", "", [], [...studyDetails])]
+  );
+});
+
+//add page title and data cards to document main section
+addToTag(main, [title, ...displayCard]);
