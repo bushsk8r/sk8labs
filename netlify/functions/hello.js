0354a0e0dcceb3e090ede5d032246cb66ef12f8f
bushsk8r <96554498+bushsk8r@users.noreply.github.com>
2026-02-20 11:16:19 -0800

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,10 +1,9 @@
-import play from './play.js';
+
 
 exports.handler = async (event, context) => {
-  const elementParam = event.path.split('/').pop;
-  const playBall = play(elementParam, 0)
+
   return {
     statusCode: 200,
-    body: JSON.stringify(playball)
+    body: JSON.stringify({message: 'hello from sk8 studio'})
   };
 };
